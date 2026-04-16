c5b58fa95e23aecd1e2a4e7c11866b33121f76c0
Peter Lee <peter.lee@astrocapital.net>
2026-04-16 02:33:14 +0000

    set larger font

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,20 +6,20 @@ local is_darwin = wezterm.target_triple:find("darwin") ~= nil
 
 -- Font and display settings
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
-config.font_size = 13
+config.font_size = 14
 config.max_fps = 240
 
 if is_windows then
-    config.default_prog = {
-        "cmd.exe",
-        "/s",
-        "/k",
-        "d:\\tool\\conda\\Scripts\\activate.bat d:\\tool\\conda & conda activate paper & d:\\lab\\paper\\winclient\\bin\\clink\\clink_x64.exe inject --profile d:\\home\\peter\\.clink",
-    }
+	config.default_prog = {
+		"cmd.exe",
+		"/s",
+		"/k",
+		"d:\\tool\\conda\\Scripts\\activate.bat d:\\tool\\conda & conda activate paper & d:\\lab\\paper\\winclient\\bin\\clink\\clink_x64.exe inject --profile d:\\home\\peter\\.clink",
+	}
 elseif is_darwin then
-    config.default_prog = { "/opt/homebrew/bin/zsh", "-l" }
+	config.default_prog = { "/opt/homebrew/bin/zsh", "-l" }
 else
-    config.default_prog = { "/usr/bin/zsh", "-l" }
+	config.default_prog = { "/usr/bin/zsh", "-l" }
 end
 
 -- Disable kitty keyboard protocol so that nvim (via tmux passthrough) cannot
@@ -31,7 +31,7 @@ config.enable_csi_u_key_encoding = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 if is_darwin then
-    config.macos_window_background_blur = 10
+	config.macos_window_background_blur = 10
 end
 config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
@@ -41,19 +41,19 @@ config.scrollback_lines = 999999
 -- Mouse selection auto-copies to clipboard
 config.selection_word_boundary = " \t\n{}[]()\"'`,;:@"
 config.mouse_bindings = {
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection"),
-    },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor("ClipboardAndPrimarySelection"),
+	},
 }
 
 -- Key bindings
 config.keys = {
-    { key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
-    { key = "+", mods = "ALT", action = wezterm.action.IncreaseFontSize },
-    { key = "=", mods = "ALT", action = wezterm.action.DecreaseFontSize },
-    { key = "n", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
+	{ key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "+", mods = "ALT", action = wezterm.action.IncreaseFontSize },
+	{ key = "=", mods = "ALT", action = wezterm.action.DecreaseFontSize },
+	{ key = "n", mods = "CTRL", action = wezterm.action.ToggleFullScreen },
 }
 
 return config
